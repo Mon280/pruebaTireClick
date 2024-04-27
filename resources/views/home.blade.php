3f87e25bc94c18dfc989e064bf7a24ff9734d8a0
Mon280 <montserrat.mr28@gmail.com>
2024-04-26 21:07:12 -0600

    plantilla

---
+++ resources/views/home.blade.php
@@ -0,0 +1,13 @@
+@extends('layouts.admin')
+
+@section('content')
+<div class="row">
+    <div class="col-md-12">
+        HOLA
+bueno 
+    </div>
+</div>
+@endsection
+@section('scripts')
+
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,8 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    if (auth()->check()) {
+        return redirect('/home');
+    }
+    return view('/home');
 });
